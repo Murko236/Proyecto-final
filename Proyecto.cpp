eb791e6fb5b2b77a33372028f8f8ba545e39fc72
ALANALEJANDRORODRIGUEZHERNANDEZ <alanrdzzhdzz@gmail.com>
2021-12-10 21:30:48 -0600

    Cambios Alan

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,1 +1,24 @@
 /*Prueba*/
+#include <stdio.h>
+void alancompararativa (int x, int y)
+{
+if  (x==y){
+	printf("Ambos son iguales");
+	}
+else {
+	if (x > y) 
+         {
+		printf("El primer numero,es mayor que el segundo numero");
+         }
+	else 
+	{
+		printf("El segundo numero,es mayor que el primero numero");
+	}
+     }
+
+}
+int alanmultplicacion (int x, int y)
+{	int res;
+	res = x * y;
+	return res;
+}
